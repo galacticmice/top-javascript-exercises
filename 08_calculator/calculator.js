304b4901793d01516e9522bc17a2f79af6a33592
ben <172694952+galacticmice@users.noreply.github.com>
2025-03-17 15:48:05 -0700

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr === undefined || arr.length == 0) {
+    return 0;
+  }
+	return arr.reduce((total, curr) => total + curr);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr === undefined || arr.length == 0) {
+    return 0;
+  }
+  return arr.reduce((total, curr) => { return total *= curr;}, 1);
 };
 
-const power = function() {
-	
+const power = function(x, p) {
+  let total = 1;
+  for (let i = 0; i < p; i++) {
+    total *= x;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+  for (let i = 1; i <= x; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
